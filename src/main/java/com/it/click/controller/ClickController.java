79a0114a937f9bb1fbe9040736e96364fd913937
Pritam <pritam.saini@indusnet.co.in>
2023-04-20 18:17:44 +0530

    base url removed again

--- src/main/java/com/it/click/controller/ClickController.java
+++ src/main/java/com/it/click/controller/ClickController.java
@@ -14,7 +14,7 @@ import com.it.click.entites.MainProfile;
 import com.it.click.service.IClickService;
 
 @RestController
-@RequestMapping("/clickit")
+@RequestMapping
 public class ClickController {
 	
 	@Autowired
