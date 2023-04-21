11cc7acad48836a3f563579bc2896f7f683515a5
Pritam <pritam.saini@indusnet.co.in>
2023-04-21 10:39:13 +0530

    Cross bean updated

--- src/main/java/com/it/click/ClickItApplication.java
+++ src/main/java/com/it/click/ClickItApplication.java
@@ -3,6 +3,7 @@ package com.it.click;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,13 +24,15 @@ public class ClickItApplication {
 //            .build();
 //    }
     
-    public WebMvcConfigurer corsConfigurer() {
-         return new WebMvcConfigurer() {
-             @Override
-             public void addCorsMappings(CorsRegistry registry) {
-                 registry.addMapping("/**").allowedOrigins("*");
-             }
-         };
-     }
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedOrigins("*")
+						.allowedHeaders("*");
+			}
+		};
+	}
 
 }
