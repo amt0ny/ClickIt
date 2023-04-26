016d93de4766215a524eb2f87ea97f20ae7b5fb6
Pritam <pritam.saini@indusnet.co.in>
2023-04-26 17:53:14 +0530

    EnableWebMvc annotation added

--- src/main/java/com/it/click/ClickItApplication.java
+++ src/main/java/com/it/click/ClickItApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableSwagger2
+@EnableWebMvc
 public class ClickItApplication {
 
 	public static void main(String[] args) {
