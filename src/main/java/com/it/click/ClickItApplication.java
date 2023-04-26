1cb7770a9c125e12ee9343f4fa3b3a5461c90685
Pritam <pritam.saini@indusnet.co.in>
2023-04-26 19:03:31 +0530

    corsConfigurer() commented from ClickItApplication

--- src/main/java/com/it/click/ClickItApplication.java
+++ src/main/java/com/it/click/ClickItApplication.java
@@ -3,17 +3,38 @@ package com.it.click;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @EnableSwagger2
+@CrossOrigin(origins = "*")
 public class ClickItApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClickItApplication.class, args);
 	}
 	
+//    @Bean
+//    public MongoClientOptions mongoOptions() {
+//        return MongoClientOptions
+//            .builder()
+//            .maxConnectionIdleTime(60000)
+//            .build();
+//    }
+    
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH").allowedOrigins("*")
+//						.allowedHeaders("*");
+//			}
+//		};
+//	}
 
 }
